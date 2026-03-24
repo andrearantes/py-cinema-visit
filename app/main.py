b1ec98174e4673a42f12ae39f41d9f3f966d5dc9
Andre Marcio <andre.marcio.ti@gmail.com>
2026-03-24 16:40:51 -0300

    verify task

--- app/main.py
+++ app/main.py
@@ -4,8 +4,8 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie:
-str) -> None:
+def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> \
+        None:
     instances_customers_list = [Customer(customer.name, customer.food)
                                 for customer in customers]
     cinema_hall = CinemaHall(hall_number)
